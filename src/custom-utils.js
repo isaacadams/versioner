1c0733fe17e8842926ce5cbe183ddafcb310c073
Isaac Adams <iadams@emoneyadvisor.com>
2019-10-24 17:25:19 -0400

    more cleaning

--- src/custom-utils.js
+++ src/custom-utils.js
@@ -76,3 +76,18 @@ export function printOutSystemEnvVars() {
         console.log(`${k}: ${process.env[k]}`);
     });
 }
+
+function findArgument(argName) {
+    let i = findFlag(argName);
+    let arg = process.argv[i + 1];
+    return arg;
+}
+
+function findFlag(argName) {
+    let i = process.argv.findIndex((v, i, o) => v === argName);
+
+    if (i < 0)
+        return undefined;
+
+    return i;
+}
