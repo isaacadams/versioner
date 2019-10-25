5a850cc3cf21fea3f595ad932bfe6c1862240a0a
Isaac Adams <iadams@emoneyadvisor.com>
2019-10-25 11:16:22 -0400

    some cleanup

--- tests.js
+++ tests.js
@@ -8,11 +8,13 @@ function importTest(name, path) {
 
 describe("versioner", function () {
     beforeEach(function () {
-       console.log("running something before each test");
+       //console.log("running something before each test");
     });
+    
     importTest("init", './src/init/tests.js');
     //importTest("b", './b/b');
+    
     after(function () {
-        console.log("after all tests");
+        //console.log("after all tests");
     });
 });
