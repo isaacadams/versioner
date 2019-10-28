5a43b8f34a2674ddb87eb94b8ef18c46fdf2ffed
Isaac Adams <iadams@emoneyadvisor.com>
2019-10-27 20:39:22 -0400

    simplified the code

--- update.sh
+++ update.sh
@@ -7,12 +7,9 @@
 IFS=
 
 output=`node.exe bin/versioner.js` # gets the usage output
-output=${output//$'\n'/@@@@} # replaces newlines with something that doesn't break the following sed command
-output=$(cat README.template.md | sed "s|>>> INSERTION POINT FOR HELP OUTPUT <<<|${output}|g") # replace usage with insertion point
-output=${output//@@@@/$'<br>'} # put newlines back in except this time double them up for .md syntax
-output=$(echo $output | sed 's|\s|\&nbsp;|g') # replace trailing spaces with html compatible ones &nbsp;
-echo $output > README.md # output to the real README.md
-#sed "s|>>> INSERTION POINT FOR HELP OUTPUT <<<|${output}|g" README.template.md > README.md
+output=${output//$'\n'/$'<br>'} # replaces newlines with something that doesn't break the following sed command
+output=$(echo $output | sed 's|\s\{2,\}|\&ensp;\&ensp;|g') # replace multiple spaces with html compatible tabs &ensp;
+sed "s|>>> INSERTION POINT FOR HELP OUTPUT <<<|${output//$'&'/$'\&'}|g" README.template.md > README.md
 
 #git add README.md
 read -p "Press enter to continue"
