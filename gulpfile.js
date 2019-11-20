02139358b6781835a9a756ac0dbd4897f8fb67fb
Isaac Adams <iadams@emoneyadvisor.com>
2019-11-20 17:03:09 -0500

    added in clean step for cleaning up dist folder before rebuilding

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 let r = require,
     gulp = r('gulp'),
     babel = r("gulp-babel"),
-    merge = r('merge-stream');
+    merge = r('merge-stream'),
+    rimraf = require('gulp-rimraf');
 
 let projects = [ CreateProject('src/**/*.js', 'dist/') ];
 
-gulp.task('build', function() {
+gulp.task('build.clean', function() {
+    return gulp.src('dist/', { read: false }) // much faster
+            //.pipe(ignore('node_modules/**'))
+            .pipe(rimraf());
+});
+
+gulp.task('build.generate', function() {
     return transpileAllProjects();
 });
+
+gulp.task('build', gulp.series([
+    'build.clean', 
+    'build.generate'
+]));
 
 function CreateProject(sourceGLOB, destination) {
     return {
